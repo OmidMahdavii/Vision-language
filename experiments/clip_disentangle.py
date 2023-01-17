c81d959eb00f96c82d363ca07653981bb0d1df33
Omid Mahdavi <s.omidmahdavi@yahoo.com>
2023-01-17 22:14:42 +0100

    Added clip disentangle

--- experiments/clip_disentangle.py
+++ experiments/clip_disentangle.py
@@ -1,4 +1,5 @@
 import torch
+import clip
 import torch.nn.functional as F
 from models.base_model import CLIPDisentangleModel
 
@@ -9,10 +10,9 @@ class HLoss(torch.nn.Module):
     def forward(self, x):
         b = F.softmax(x, dim=1) * F.log_softmax(x, dim=1)
         return b.sum() / x.size(0)
+
+
 class CLIPDisentangleExperiment: # See point 4. of the project
-    
-
-
     
     def __init__(self, opt):
         # Utils
@@ -26,11 +26,18 @@ class CLIPDisentangleExperiment: # See point 4. of the project
         for param in self.model.parameters():
             param.requires_grad = True
 
+        # Load CLIP model and freeze it
+        self.clip_model, _ = clip.load('ViT-B/32', device='cpu') # load it first to CPU to ensure you're using fp32 precision.
+        self.clip_model = self.clip_model.to(self.device)
+        self.clip_model.eval()
+        for param in self.clip_model.parameters():
+            param.requires_grad = False
+
         # Setup optimization procedure
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=opt['lr'])
         self.cross_entropy_criterion = torch.nn.CrossEntropyLoss()
         self.entropy_criterion = HLoss()
-        self.reconstruction_criterion = torch.nn.MSELoss()
+        self.L2_criterion = torch.nn.MSELoss()
         # self.KLDiv_criterion = torch.nn.KLDivLoss()
 
     def save_checkpoint(self, path, iteration, best_accuracy, total_train_loss):
@@ -57,10 +64,18 @@ class CLIPDisentangleExperiment: # See point 4. of the project
 
         return iteration, best_accuracy, total_train_loss
 
+    def text_feature_extractor(self, description):
+        # use the textual encoder:
+        tokenized_text = clip.tokenize(description).to(self.device)
+        text_features = self.clip_model.encode_text(tokenized_text)
+
+        return text_features
+
     def train_iteration(self, data):
-        x, y = data
+        x, y, description = data
 
         source_indices = [i for i in range(len(y)) if y[i] != 7]
+        clip_indices = [i for i in range(len(description)) if len(description[i]) != 0]
         
         domain_labels = torch.ones(y.size(), dtype=torch.long)
         domain_labels[source_indices] = 0.0
@@ -71,6 +86,10 @@ class CLIPDisentangleExperiment: # See point 4. of the project
         # only source samples are going through category classifier
         source_samples = x[source_indices].to(self.device)
         category_labels = y[source_indices].to(self.device)
+
+        # only samples which have clip label are going through text encoder
+        clip_samples = x[clip_indices].to(self.device)
+        clip_description = [description[i] for i in clip_indices]
 
         self.optimizer.zero_grad()
         loss = 0
@@ -99,11 +118,19 @@ class CLIPDisentangleExperiment: # See point 4. of the project
         loss += temp_loss
 
         logits = self.model(x, status='rc')
-        temp_loss = self.reconstruction_criterion(*logits)
+        temp_loss = self.L2_criterion(*logits)
         # temp_loss2 = self.KLDiv_criterion(*logits) / 4
         # temp_loss = temp_loss1 + temp_loss2
         temp_loss.backward()
         loss += temp_loss
+
+        # the batch should have more than 1 clip sample to be able to compute the loss
+        if len(clip_indices) > 1:
+            logits = self.model(clip_samples, status='df')
+            text_features = self.text_feature_extractor(clip_description)
+            temp_loss = self.L2_criterion(logits, text_features)
+            temp_loss.backward()
+            loss += temp_loss
 
         self.optimizer.step()
     
@@ -115,7 +142,7 @@ class CLIPDisentangleExperiment: # See point 4. of the project
         count = 0
         loss = 0
         with torch.no_grad():
-            for x, y in loader:
+            for x, y, _ in loader:
                 source_indices = [i for i in range(len(y)) if y[i] != 7]
                 domain_labels = torch.ones(y.size(), dtype=torch.long)
                 domain_labels[source_indices] = 0.0
@@ -142,7 +169,7 @@ class CLIPDisentangleExperiment: # See point 4. of the project
                     # the batch should have more than 1 source sample to be able to classify the category
                     if len(source_indices) > 1:
                         logits = self.model(source_samples, status='cc', minimizing=True)
-                        loss += 2 * self.cross_entropy_criterion(logits, category_labels)
+                        loss += self.cross_entropy_criterion(logits, category_labels)
                         pred = torch.argmax(logits, dim=-1)
                         accuracy += (pred == category_labels).sum().item()
                         count += source_samples.size(0)
